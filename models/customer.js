d6787d0268ba11295e0ebc15bc7844ba200e4bb9
Fasil Minale <fasiladam1290@gmail.com>
2020-11-14 07:05:18 +0300

    customer model.

--- models/customer.js
+++ models/customer.js
@@ -1,51 +1,50 @@
-const mongoose = require('mongoose')
-const Joi = require('joi')
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const customerSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    },
-    lastName: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255
-    },
-    phone: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    },
-    city: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50
-    }
-})
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  content: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
 
-const Customer = mongoose.model('Customer', customerSchema)
+const Customer = mongoose.model("Customer", customerSchema);
 
 function validateCustomer(customer) {
-    const schema = {
-        firstName: Joi.string().min(5).max(50).required(),
-        lastName: Joi.string().min(5).max(50).required(),
-        email: Joi.string().min(5).max(255).required(),
-        phone: Joi.string().min(5).max(50).required(),
-        city: Joi.string().min(5).max(50).required()
-    }
+  const schema = {
+    firstName: Joi.string().min(5).max(50).required(),
+    lastName: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required(),
+    phone: Joi.string().min(5).max(50).required(),
+    city: Joi.string().min(5).max(50).required(),
+  };
 
-    return Joi.validate(customer, schema)
+  return Joi.validate(customer, schema);
 }
 
 exports.Customer = Customer;
