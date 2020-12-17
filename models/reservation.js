3a47075e03a18b69902d571498f240bbc581e812
Fasil Minale <fasiladam1290@gmail.com>
2020-12-17 04:30:37 +0300

    some changes

--- models/reservation.js
+++ models/reservation.js
@@ -1,73 +1,63 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-
-const roomSchema = require("./room");
-const customerSchema = require("./customer");
-const hotelSchema = require("./hotel");
+const { roomSchema } = require("./room");
+const { userSchema } = require("./user");
+const { hotelSchema } = require("./hotel");
 
 const reservationSchema = new mongoose.Schema({
-    bookingNumber: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 254,
-    },
-    checkInDate: {
-        type: Date,
-        required: true,
-    },
-    checkOutDate: {
-        type: Date,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    bookedOnDate: {
-        type: Date,
-        required: true,
-    },
-    numberOfGuest: {
-        type: Number,
-        required: true,
-    },
-    status: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 254,
-    },
-
-    costomer: {
-        type: customerSchema,
-    },
-    room: {
-        type: roomSchema,
-    },
-    hotel: {
-        type: hotelSchema,
-    },
+  confirmationNumber: {
+    type: String,
+    required: true,
+  },
+  checkInDate: {
+    type: Date,
+    required: true,
+  },
+  checkOutDate: {
+    type: Date,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  bookedOnDate: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Pending",
+    required: true,
+  },
+  customer: {
+    type: userSchema,
+  },
+  room: {
+    type: roomSchema,
+  },
+  hotel: {
+    type: hotelSchema,
+  },
 });
 
 const Reservation = mongoose.model("Reservation", reservationSchema);
 
 function validateReservation(reservation) {
-    const schema = {
-        bookingNumber: Joi.string().min(1).max(254).required(),
-        checkInDate: Joi.date().required(),
-        checkOutDate: Joi.date().required(),
-        price: Joi.number().required(),
-        bookedOnDate: Joi.date().required(),
-        numberOfGuest: Joi.number().required(),
-        status: Joi.string().min(1).max(254).required(),
-        costomerId: Joi.objectId().required(),
-        roomId: Joi.objectId().required(),
-        hotelId: Joi.objectId().required(),
-    };
-    return Joi.validate(reservation, schema);
+  const schema = {
+    confirmationNumber: Joi.string().min(1).max(254).required(),
+    checkInDate: Joi.date().required(),
+    checkOutDate: Joi.date().required(),
+    status: Joi.string(),
+    userId: Joi.objectId(),
+    roomId: Joi.objectId(),
+    hotelId: Joi.objectId(),
+  };
+  return Joi.validate(reservation, schema);
 }
 
+exports.reservationSchema = reservationSchema;
 exports.Reservation = Reservation;
 exports.validate = validateReservation;
--- routes/reservations.js
+++ routes/reservations.js
@@ -1,85 +1,105 @@
-const mongoose = require('mongoose');
-const { Reservation, validate } = require('../models/reservation');
+const mongoose = require("mongoose");
+const { Reservation, validate } = require("../models/reservation");
 const express = require("express");
 const router = express.Router();
+const { Room } = require("../models/room");
+const { User } = require("../models/user");
+const { Hotel } = require("../models/hotel");
+const moment = require("moment");
 
-router.get("/", async(req, res) => {
-    const reservation = await Reservation.find().sort('code')
-    res.send(reservation);
+router.get("/", async (req, res) => {
+  const reservation = await Reservation.find().sort("code");
+  res.send(reservation);
 });
 
-router.get("/:id", async(req, res) => {
-    // Lookup
-    // If not existing, return 404
-    const reservation = await Reservation.findById(req.params.id)
-    if (!reservation)
-        return res.status(404).send("The reservation with the given ID was not found.");
+router.get("/:id", async (req, res) => {
+  // Lookup
+  // If not existing, return 404
+  const reservation = await Reservation.findById(req.params.id);
+  if (!reservation)
+    return res
+      .status(404)
+      .send("The reservation with the given ID was not found.");
 
-    res.send(reservation);
+  res.send(reservation);
 });
 
-router.post("/", async(req, res) => {
-    // Validate
-    // If invalid, return 400 - Bad request
-    const { error } = validate(req.body)
-    if (error)
-        return res.status(400).send(error.details[0].message)
+router.post("/", async (req, res) => {
+  // Validate
+  // If invalid, return 400 - Bad request
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const room = await Room.findById(req.body.roomId);
+  if (!room) return res.status(400).send("Invalid Room.");
+  const user = await User.findById(req.body.userId);
+  if (!user) return res.status(400).send("Invalid User.");
+  const hotel = await Hotel.findById(req.body.hotelId);
+  if (!hotel) return res.status(400).send("Invalid Hotel.");
 
-    const reservation = new Reservation({
-        bookingNumber: req.body.bookingNumber,
-        checkInDate: req.body.checkInDate,
-        checkOutDate: req.body.checkOutDate,
-        price: req.body.price,
-        bookedOnDate: req.body.bookedOnDate,
-        numberOfGuest: req.body.numberOfGuest,
-        status: req.body.status,
-        costomer: req.body.costomer,
-        room: req.body.room,
-        hotel: req.body.hotel,
-    });
-    awaitreservation.save()
-    res.send(reservation);
+  var start = moment(req.body.checkInDate);
+  var end = moment(req.body.checkOutDate);
+  dateDiff = end.diff(start, "days");
+  var price = dateDiff * room.price;
+
+  const reservation = new Reservation({
+    confirmationNumber: req.body.confirmationNumber,
+    checkInDate: req.body.checkInDate,
+    checkOutDate: req.body.checkOutDate,
+    price: price,
+    customer: user,
+    room: room,
+    hotel: hotel,
+  });
+  await reservation.save();
+  res.send(reservation);
 });
 
-router.put("/:id", async(req, res) => {
-    // Validate
-    // If invalid, return 400 - Bad request
-    const { error } = validate(req.body)
-    if (error)
-        return res.status(400).send(error.details[0].message)
+router.put("/:id", async (req, res) => {
+  // Validate
+  // If invalid, return 400 - Bad request
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    // Lookup and update
-    // If not existing, return 404
-    const reservation = await Reservation.findByIdAndUpdate(req.params.id, {
-        bookingNumber: req.body.bookingNumber,
-        checkInDate: req.body.checkInDate,
-        checkOutDate: req.body.checkOutDate,
-        price: req.body.price,
-        bookedOnDate: req.body.bookedOnDate,
-        numberOfGuest: req.body.numberOfGuest,
-        status: req.body.status,
-        costomer: req.body.costomer,
-        room: req.body.room,
-        hotel: req.body.hotel,
-    }, { new: true })
+  // Lookup and update
+  // If not existing, return 404
+  const reservation = await Reservation.findByIdAndUpdate(
+    req.params.id,
+    {
+      bookingNumber: req.body.bookingNumber,
+      checkInDate: req.body.checkInDate,
+      checkOutDate: req.body.checkOutDate,
+      price: req.body.price,
+      bookedOnDate: req.body.bookedOnDate,
+      numberOfGuest: req.body.numberOfGuest,
+      status: req.body.status,
+      costomer: req.body.costomer,
+      room: req.body.room,
+      hotel: req.body.hotel,
+    },
+    { new: true }
+  );
 
-    if (!reservation)
-        return res.status(404).send("The reservation with the given ID was not found.");
+  if (!reservation)
+    return res
+      .status(404)
+      .send("The reservation with the given ID was not found.");
 
-    // Return the updated reservation
-    res.status(200).send(reservation)
+  // Return the updated reservation
+  res.status(200).send(reservation);
 });
 
-router.delete("/:id", async(req, res) => {
-    //Lookup the reservation and remove
-    //Not existing,return 404
-    const reservation = await Reservation.findByIdAndRemove(req.params.id)
+router.delete("/:id", async (req, res) => {
+  //Lookup the reservation and remove
+  //Not existing,return 404
+  const reservation = await Reservation.findByIdAndRemove(req.params.id);
 
-    if (!reservation)
-        return res.status(404).send("The reservation with the given ID was not found.");
+  if (!reservation)
+    return res
+      .status(404)
+      .send("The reservation with the given ID was not found.");
 
-    //return the reservation that was deleted.
-    res.send(reservation)
+  //return the reservation that was deleted.
+  res.send(reservation);
 });
 
-module.exports = router
+module.exports = router;
